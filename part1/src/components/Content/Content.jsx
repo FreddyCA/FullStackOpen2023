d56bdedde2959cd62b059c7341bafdf966c97180
Freddy Coaricona <fcoaricona@outlook.com>
2023-07-12 21:04:46 -0400

    Exercise 1.3: course information, step3 - Modify the variable, Refactor the application : Completed

--- part1/src/components/Content/Content.jsx
+++ part1/src/components/Content/Content.jsx
@@ -3,20 +3,17 @@ import PropTypes from "prop-types";
 const Content = (props) => {
     return (
         <>
-            <p>{props.part1} {props.exercises1}</p>
-            <p>{props.part2} {props.exercises2}</p>
-            <p>{props.part3} {props.exercises3}</p>
+            <p>{props.part1.name} {props.part1.exercises}</p>
+            <p>{props.part2.name} {props.part2.exercises}</p>
+            <p>{props.part3.name} {props.part3.exercises}</p>
         </>
     )
 }
 
 Content.propTypes = {
-    part1: PropTypes.string.isRequired,
-    exercises1: PropTypes.number.isRequired,
-    part2: PropTypes.string.isRequired,
-    exercises2: PropTypes.number.isRequired,
-    part3: PropTypes.string.isRequired,
-    exercises3: PropTypes.number.isRequired,
+    part1: PropTypes.object.isRequired,
+    part2: PropTypes.object.isRequired,
+    part3: PropTypes.object.isRequired,
 };
 
 export default Content
