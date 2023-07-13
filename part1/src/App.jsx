93c8b589a025cff25006d0acb515e456fee2efa9
Freddy Coaricona <fcoaricona@outlook.com>
2023-07-13 19:52:47 -0400

    Exercise 1.4: course information, step4 - Objects into an Array , Refactor the application : Completed

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,27 +4,36 @@ import Header from "./components/Header/Header"
 const App = () => {
   const course = 'Half Stack application development'
 
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  const exercisesCounts = parts.map(value => value.exercises)
+
+  let sum = 0;
+  exercisesCounts.forEach((value) => {
+    sum += value
+  })
 
   return (
     <div>
     
       <Header nameCourse={course} />
 
-      <Content part1={part1} part2={part2} part3={part3} />
+      <Content parts={parts} />
 
-      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
+      <p>Number of exercises {sum}</p>
     </div>
   )
 }
--- part1/src/components/Content/Content.jsx
+++ part1/src/components/Content/Content.jsx
@@ -1,19 +1,19 @@
 import PropTypes from "prop-types";
 
 const Content = (props) => {
+
+    const content = props.parts.map((value,index) => <p key={index}>{value.name} {value.exercises}</p>)
+    
     return (
         <>
-            <p>{props.part1.name} {props.part1.exercises}</p>
-            <p>{props.part2.name} {props.part2.exercises}</p>
-            <p>{props.part3.name} {props.part3.exercises}</p>
+        {content}
         </>
     )
 }
 
 Content.propTypes = {
-    part1: PropTypes.object.isRequired,
-    part2: PropTypes.object.isRequired,
-    part3: PropTypes.object.isRequired,
+    parts: PropTypes.array.isRequired,
+    
 };
 
 export default Content
