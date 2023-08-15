6622edf0a4999f8269f09153bc64ac181cbfe1d4
Freddy Coaricona <fcoaricona@outlook.com>
2023-08-14 20:29:55 -0400

    Exercise 1.8: unicafe, step3 - Refactor Statistics component : Completed

--- part1-unicafe/src/components/Statistics/Statistics.jsx
+++ part1-unicafe/src/components/Statistics/Statistics.jsx
@@ -1,40 +1,46 @@
 import PropTypes from "prop-types";
 
 const TitleStatistics = () => {
-    return <h2>statistics</h2>
-}
+  return <h2>statistics</h2>;
+};
 
-const Characteristics = ({variable, name}) => {
-    return <p>{name}: {variable}</p>
-}
+const Characteristics = ({ variable, name, percentage }) => {
+  return (
+    <p>
+      {name}: {percentage ? variable + " %" : variable} 
+    </p>
+  );
+};
 
-const Statistics = ({good, neutral, bad, name}) => {
-    const all = good + neutral + bad
-    const average = (good - bad) / all
-    const positive = (good / all) * 100
+const Statistics = ({ good, neutral, bad, name }) => {
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100;
 
-    return <div>
-        <TitleStatistics />
-        <Characteristics variable ={good} name={name.good} />
-        <Characteristics variable ={neutral} name={name.neutral} />
-        <Characteristics variable ={bad} name={name.bad} />
-        <Characteristics variable={all} name="all"/>
-        <Characteristics variable={isNaN(average) ? 0 : average} name="average"/>
-        <Characteristics variable={isNaN(positive) ? 0 : positive} name="positive"/>
+  return (
+    <div>
+      <TitleStatistics />
+      <Characteristics name={name.good} variable={good} />
+      <Characteristics name={name.neutral} variable={neutral} />
+      <Characteristics name={name.bad} variable={bad} />
+      <Characteristics name="all" variable={all} />
+      <Characteristics name="average" variable={isNaN(average) ? 0 : average} />
+      <Characteristics name="positive" variable={isNaN(positive) ? 0 : positive} percentage={true} />
     </div>
-}
-
+  );
+};
 
 Statistics.propTypes = {
-    good: PropTypes.number,
-    neutral: PropTypes.number,
-    bad: PropTypes.number,
-    name: PropTypes.object.isRequired,
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  name: PropTypes.object.isRequired,
 };
 
 Characteristics.propTypes = {
-    variable: PropTypes.number,
-    name: PropTypes.string
+  variable: PropTypes.number,
+  name: PropTypes.string,
+  percentage: PropTypes.bool,
 };
 
-export default Statistics
+export default Statistics;
