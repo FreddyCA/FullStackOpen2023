90c3a7aec74ad3149b13bd093ad778c2a337fce2
Freddy Coaricona <fcoaricona@outlook.com>
2023-08-17 21:16:14 -0400

    Exercise 2.8: The Phonebook, step3 - Add phone numbres to the phone book : Completed

--- part2/part2.2-the_phonebook/src/App.jsx
+++ part2/part2.2-the_phonebook/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
 
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
+  };
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
   };
 
   const addName = (e) => {
@@ -15,11 +22,11 @@ const App = () => {
 
     if (!duplicateName) {
       if (newName.trim() !== "") {
-        const newPerson = { name: newName };
+        const newPerson = { name: newName, number: newNumber };
         setPersons((allPersons) => [...allPersons, newPerson]);
       }
     } else {
-      alert(`${newName} is already added to phonebook`)
+      alert(`${newName} is already added to phonebook`);
     }
   };
 
@@ -32,12 +39,22 @@ const App = () => {
           <input id="name" value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          <label htmlFor="number">number:</label>
+          <input
+            id="number"
+            value={newNumber}
+            onChange={handleNumberChange}
+          ></input>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person, index) => (
-        <p key={person.name + "-" + index}>{person.name}</p>
+        <p key={person.name + "-" + index}>
+          {person.name} {person.number}
+        </p>
       ))}
     </div>
   );
