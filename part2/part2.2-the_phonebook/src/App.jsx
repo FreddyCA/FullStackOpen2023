7cb2f1e9334356d8b085821bf504e7fa14598bc7
Freddy Coaricona <fcoaricona@outlook.com>
2023-08-17 23:35:34 -0400

    Exercise 2.9: The Phonebook, step4 - Search field with filter implemented : Completed

--- part2/part2.2-the_phonebook/src/App.jsx
+++ part2/part2.2-the_phonebook/src/App.jsx
@@ -2,11 +2,18 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
 
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterPerson, setFilterPerson] = useState("");
+
+  const [filterAll, setFilterAll] = useState([]);
+  const [filterWatch, setFilterWatch] = useState(false);
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
@@ -16,14 +23,35 @@ const App = () => {
     setNewNumber(e.target.value);
   };
 
+  const handleFilterChange = (e) => {
+    const nameFilter = e.target.value;
+    setFilterPerson(nameFilter);
+    filter(nameFilter);
+  };
+
+  const filter = (name) => {
+    if (name.length > 2) {
+      const filterPersons = persons.filter((person) =>
+        person.name.toLowerCase().includes(name.toLowerCase())
+      );
+      setFilterAll(filterPersons);
+      setFilterWatch(filterPersons.length > 0);
+    } else {
+      setFilterAll([]);
+      setFilterWatch(false);
+    }
+  };
+
   const addName = (e) => {
     e.preventDefault();
     const duplicateName = persons.some((person) => person.name === newName);
 
     if (!duplicateName) {
-      if (newName.trim() !== "") {
+      if (newName.trim() !== "" && newNumber.trim() !== "") {
         const newPerson = { name: newName, number: newNumber };
         setPersons((allPersons) => [...allPersons, newPerson]);
+        setNewName("");
+        setNewNumber("");
       }
     } else {
       alert(`${newName} is already added to phonebook`);
@@ -33,6 +61,26 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <label htmlFor="filter">filter shown with</label>
+        <input
+          id="filter"
+          value={filterPerson}
+          onChange={handleFilterChange}
+        ></input>
+      </div>
+
+      {filterWatch && (
+        <div>
+          {filterAll.map((filter, index) => (
+            <p key={filter.name + "-" + index}>
+              {filter.name} {filter.number}
+            </p>
+          ))}
+        </div>
+      )}
+
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           <label htmlFor="name">name:</label>
